4f210149f65625996845c993a072c43ad8b95377
unknown <medmoudyacoub4@gmail.com>
2025-01-20 11:07:24 -0800

    more features

--- author_files/myql.sql
+++ author_files/myql.sql
@@ -221,7 +221,7 @@ SELECT * FROM niveau;
 SELECT * FROM departement;
 SELECT * FROM semestre;
 SELECT * FROM cours ORDER BY code_sem;
-SELECT * FROM etudiants WHERE matricule = "23602@isms.esp.mr" order by (lib_annee_univ) DESC;
+SELECT * FROM etudiants WHERE matricule = "23618@isms.esp.mr" order by (lib_annee_univ) DESC;
 SELECT * FROM evaluer;
 SELECT * FROM images;
 SELECT * FROM section;
